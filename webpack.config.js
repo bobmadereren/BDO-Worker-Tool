ed9488bbf38565dd5fc3713390bb98ae841731d8
bobmadereren <williamspurkeland@hotmail.com>
2024-11-21 10:32:29 +0100

    setup environment for building with webpack

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const entry = './src/index.js';
+
+const output = {
+    filename: 'bundle[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+}
+
+const devtool = 'source-map';
+
+const devServer = {
+    static: {
+        directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 666,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+}
+
+const mode = 'development';
+
+const module = {
+    rules: [
+        {
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        }
+    ]
+}
+
+const plugins = [
+    new HtmlWebpackPlugin({
+        title: 'BDO Worker Tool!',
+        filename: 'index.html',
+        template: 'src/index.html'
+    }),
+];
+
+export default { entry, output, devtool, devServer, mode, module, plugins };
