34a8a6f24ab84746621dedf2bcd585897937f529
bobmadereren <williamspurkeland@hotmail.com>
2024-11-27 10:57:46 +0100

    cleaning build configurations

--- webpack.config.js
+++ webpack.config.js
@@ -5,56 +5,44 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const entry = './src/index.js';
-
-const output = {
-    filename: 'bundle[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-}
-
-const devtool = 'source-map';
-
-const devServer = {
-    static: {
-        directory: path.resolve(__dirname, 'dist'),
+export default {
+    entry: './src/index.js',
+    
+    output: {
+        filename: 'bundle[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
-    port: 666,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
-}
 
-const mode = 'development';
+    devServer: {
+        port: 666,
+        open: true,
+        hot: true,
+        compress: true,
+    },
 
-const module = {
-    rules: [
-        {
-            test: /\.css$/,
-            use: [
-                'style-loader',
-                'css-loader'
-            ]
-        },
-        {
-            test: /\.png$/i,
-            type: 'asset/resource'
-        },
-        {
-            test: /\.ts$/,
-            use: 'ts-loader',
-            exclude: /node_modules/
-        }
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'BDO Worker Tool!',
+            filename: 'index.html',
+            template: 'src/index.html'
+        }),
     ]
-}
-
-const plugins = [
-    new HtmlWebpackPlugin({
-        title: 'BDO Worker Tool!',
-        filename: 'index.html',
-        template: 'src/index.html'
-    }),
-];
-
-export default { entry, output, devtool, devServer, mode, module, plugins };
+};
